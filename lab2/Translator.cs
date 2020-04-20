372d8d22fd3ba01c489cbc5d5a430c4a4c4824e9
just_ort <dr.v991149@gmail.com>
2020-04-20 22:14:48 +0300

    (fix): russian dictionary

--- lab2/Translator.cs
+++ lab2/Translator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -9,9 +8,9 @@ namespace lab2
     {
         private static Dictionary<Keys, string> dictionary = new Dictionary<Keys, string>
         {
-            {Keys.Q, "Й"}, {Keys.W, "Ц"}, {Keys.R, "К"}, {Keys.T, "Е"}, {Keys.Y, "Н"}, {Keys.U, "Е"}, {Keys.E, "У"},
+            {Keys.Q, "Й"}, {Keys.W, "Ц"}, {Keys.R, "К"}, {Keys.T, "Е"}, {Keys.Y, "Н"}, {Keys.U, "Г"}, {Keys.E, "У"},
             {Keys.O, "Щ"}, {Keys.P, "З"}, {Keys.I, "Ш"}, {Keys.OemOpenBrackets, "Х"}, {Keys.Oem6, "Ъ"}, {Keys.Oem1, "Ж"},
-            {Keys.Oem7, "Э"}, {Keys.Oemcomma, "Б"}, {Keys.OemPeriod, "."}, {Keys.A, "Ф"}, {Keys.S, "Ы"}, {Keys.D, "В"}, 
+            {Keys.Oem7, "Э"}, {Keys.Oemcomma, "Б"}, {Keys.OemPeriod, "Ю"}, {Keys.A, "Ф"}, {Keys.S, "Ы"}, {Keys.D, "В"}, 
             {Keys.F, "А"}, {Keys.G, "П"}, {Keys.H, "Р"}, {Keys.J, "О"}, {Keys.K, "Л"}, {Keys.L, "Д"}, {Keys.Z, "Я"}, 
             {Keys.X, "Ч"}, {Keys.C, "С"}, {Keys.V, "М"}, {Keys.B, "И"}, {Keys.N, "Т"}, {Keys.M, "Ь"}
         };
